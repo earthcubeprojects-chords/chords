916d24fc91d4c6e2431fa84b9088d2201d9c2307
Mike Dye <mdye@ucar.edu>
2015-04-25 22:59:39 -0600

    user files migration removed

--- db/migrate/20150418145607_add_columns_to_user_files.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToUserFiles < ActiveRecord::Migration
-  def change
-    add_column :user_files, :name, :string
-    add_column :user_files, :function, :string
-    add_column :user_files, :data, :binary
-  end
-end
