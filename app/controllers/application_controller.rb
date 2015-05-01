a77b9690f477347951ae6eee429526d03dbe81e4
Charlie Martin <martinc@ucar.edu>
2015-04-30 21:27:18 -0600

    Display the project name in the navigation header.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_filter :set_before_profile
+  
+  def set_before_profile
+    @before_profile = Profile.find(1)
+  end
+  
 end
