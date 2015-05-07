2b312c634f66a09ebd1418d80a3639a8d791c062
Mike Dye <mdye@ucar.edu>
2015-05-07 14:41:51 -0600

    seed sites and instruments added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Site.create([{name: 'ISS1 Lyons', lat: '40.2', lon:'-104.0'}])
+Site.create([{name: 'ISS2 Sugarloaf', lat: '40.0', lon:'-104.1'}])
+Site.create([{name: 'ISS3 Nederland', lat: '39.9', lon:'-104.3'}])
+
+Instrument.create([{name: 'Campbell', site_id:'1', }])
+Instrument.create([{name: 'Campbell', site_id:'2', }])
+Instrument.create([{name: 'Campbell', site_id:'3', }])
+
+Instrument.create([{name: '449 Profiler', site_id:'2', }])
+
+Instrument.create([{name: '915 Profiler', site_id:'1', }])
+Instrument.create([{name: '915 Profiler', site_id:'3', }])
