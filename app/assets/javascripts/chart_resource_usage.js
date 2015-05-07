619357b4a6f5465f432e75ab64ec8a6323338286
Charlie Martin <martinc@ucar.edu>
2015-05-07 15:29:45 -0600

    Move javascript charts into individual javascript files.

---
+++ app/assets/javascripts/chart_resource_usage.js
@@ -0,0 +1,100 @@
+$(function () { 
+    $('#chart_resource_usage').highcharts({
+        chart: {
+            type: 'gauge',
+            alignTicks: false,
+            plotBackgroundColor: null,
+            plotBackgroundImage: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+        },
+    
+        title: {
+            text: null
+        },
+        
+        pane: {
+            startAngle: -150,
+            endAngle: 150
+        },            
+    
+        yAxis: [{
+            min: 0,
+            max: 200,
+            lineColor: '#339',
+            tickColor: '#339',
+            minorTickColor: '#339',
+            offset: -25,
+            lineWidth: 2,
+            labels: {
+                distance: -20,
+                rotation: 'auto'
+            },
+            tickLength: 5,
+            minorTickLength: 5,
+            endOnTick: false
+        }, {
+            min: 0,
+            max: 124,
+            tickPosition: 'outside',
+            lineColor: '#933',
+            lineWidth: 2,
+            minorTickPosition: 'outside',
+            tickColor: '#933',
+            minorTickColor: '#933',
+            tickLength: 5,
+            minorTickLength: 5,
+            labels: {
+                distance: 12,
+                rotation: 'auto'
+            },
+            offset: -20,
+            endOnTick: false
+        }],
+    
+        series: [{
+            name: 'Requests',
+            data: [80],
+            dataLabels: {
+                formatter: function () {
+                    var kmh = this.y,
+                        mph = Math.round(kmh * 0.621);
+                    return '<span style="color:#339">'+ kmh + '</span><br/>' +
+                        '<span style="color:#933">' + mph + '</span>';
+                },
+                backgroundColor: {
+                    linearGradient: {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
+                    },
+                    stops: [
+                        [0, '#DDD'],
+                        [1, '#FFF']
+                    ]
+                }
+            },
+            tooltip: {
+                valueSuffix: '/s'
+            }
+        }]
+    
+    },
+    // Add some life
+    function(chart) {
+        setInterval(function() {
+            var point = chart.series[0].points[0],
+                newVal, inc = Math.round((Math.random() - 0.5) * 20);
+    
+            newVal = point.y + inc;
+            if (newVal < 0 || newVal > 200) {
+                newVal = point.y - inc;
+            }
+    
+            point.update(newVal);
+    
+        }, 3000);
+    
+    });
+});	
