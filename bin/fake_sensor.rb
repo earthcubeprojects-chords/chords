8fb9c5d57e07a6f9c23e353a0a821cc50fe61afe
Mike Dye <mdye@ucar.edu>
2015-05-12 22:58:06 -0700

    fake sensor working

--- bin/fake_sensor.rb
+++ bin/fake_sensor.rb
@@ -4,12 +4,13 @@ require 'net/http'
 
 # get 'measurements/url_create/:instrument_id/:parameter/:value/:unit' => 'measurements#url_create'
 
-last_temp = 15
-while 1 do 
+current_temp = 15
+while 1 do  
+  rand = Random.rand(5.0) / 100 
 
-  temp_diff = Random.rand(0.05) - 0.1
-  current_temp = last_temp + temp_diff
-
+  temp_diff = rand - 0.025
+  current_temp = current_temp + temp_diff
+# puts "#{rand} #{temp_diff} #{current_temp}"
 # puts temp_diff 
 encoded = URI::encode(current_temp.to_s)
   url = URI.parse("http://localhost:3000/measurements/url_create/1?parameter=temperature&value=#{encoded}&unit=C")
@@ -18,7 +19,7 @@ encoded = URI::encode(current_temp.to_s)
   res = Net::HTTP.start(url.host, url.port) {|http|
     http.request(req)
   }
-  # puts res.body
+#   # puts res.body
   sleep(1)
 end
 
