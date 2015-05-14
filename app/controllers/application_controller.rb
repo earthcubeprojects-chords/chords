fdf599525b2a1f46962e331e50f3c1625516095b
Mike Dye <mdye@ucar.edu>
2015-05-14 13:11:39 -0700

    samples per hour working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,24 @@ class ApplicationController < ActionController::Base
   def set_before_profile
     @before_profile = Profile.find(1)
 
-    @measurements_by_minute = Measurement.group("date_format(created_at, '%Y/%m/%d %H:%i')").count
-    @measurements_by_hour = Measurement.group("date_format(created_at, '%Y/%m/%d %H')").count
+    @measurements_by_minute = Measurement.group("date_format(created_at, '%Y-%m-%dT%H:%i')").count
+    @measurements_by_hour = Measurement.group("date_format(created_at, '%Y-%m-%dT%H')").count
+    
+    
 
+    @measurements_by_hour_ms = Hash.new
+    data_array = Array.new
+    @measurements_by_hour.each do |measurement_by_hour| 
+
+      iso8601 =      measurement_by_hour[0] + ':00:00+07:00'
+      miliseconds = (Time.iso8601(iso8601).to_f * 1000.0).to_i
+      
+      str = '[' + miliseconds.to_s + ','+ measurement_by_hour[1].to_s + ']'
+      data_array.push(str)
+      
+    end
+    
+    @measurements_by_hour_data = data_array.join(',')
   end
   
 end
