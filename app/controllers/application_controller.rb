80d78911ce7dafdc622a06ce01578cc1124d9671
Mike Dye <mdye@ucar.edu>
2015-05-14 13:41:06 -0700

    samples per minute working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,21 +8,32 @@ class ApplicationController < ActionController::Base
   def set_before_profile
     @before_profile = Profile.find(1)
 
-    @measurements_by_minute = Measurement.group("date_format(created_at, '%Y-%m-%dT%H:%i')").count
-    @measurements_by_hour = Measurement.group("date_format(created_at, '%Y-%m-%dT%H')").count
-    
-    
+    # @measurements_by_minute = Measurement.group("date_format(created_at, '%Y-%m-%dT%H:%i')").count
 
-    @measurements_by_hour_ms = Hash.new
+    @measurements_by_minute = Measurement.group("date_format(created_at, '%Y-%m-%dT%H:%i')").count
+
+    data_array = Array.new
+    @measurements_by_minute.each do |measurement_by_minute| 
+      iso8601 =      measurement_by_minute[0] + ':00+07:00'
+      miliseconds = (Time.iso8601(iso8601).to_f * 1000.0).to_i
+      str = '[' + miliseconds.to_s + ','+ measurement_by_minute[1].to_s + ']'
+      data_array.push(str)
+    end
+    
+    @measurements_by_minute_data = data_array.join(',')
+
+
+
+
+
+    @measurements_by_hour = Measurement.group("date_format(created_at, '%Y-%m-%dT%H')").count
+
     data_array = Array.new
     @measurements_by_hour.each do |measurement_by_hour| 
-
       iso8601 =      measurement_by_hour[0] + ':00:00+07:00'
       miliseconds = (Time.iso8601(iso8601).to_f * 1000.0).to_i
-      
       str = '[' + miliseconds.to_s + ','+ measurement_by_hour[1].to_s + ']'
       data_array.push(str)
-      
     end
     
     @measurements_by_hour_data = data_array.join(',')
