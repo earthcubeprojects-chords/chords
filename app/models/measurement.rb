c5936649dac2daddb3c615453fb14d9deab69de8
Mike Dye <mdye@ucar.edu>
2015-05-20 13:01:33 -0600

    cdv download added for measurements

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,3 +1,14 @@
 class Measurement < ActiveRecord::Base
   belongs_to :instrument
+
+
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |measurement|
+        csv << measurement.attributes.values_at(*column_names)
+      end
+    end
+  end
+
 end
