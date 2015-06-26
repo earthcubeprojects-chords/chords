3a24257677c8a5ebf949db334cdeddc7ecbb6713
Charlie Martin <martinc@ucar.edu>
2015-06-25 18:03:38 -0600

    Clean up the database size caclulation.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,32 @@
 module ApplicationHelper
 
+  # return the database size in MB.
   def self.total_db_size_mb
+    # Create a query to determine the database size. Use the information_schema
+    # table, summing data index totals for disk based tables that are using our
+    # database.
     sql = "SELECT
              sum( data_length + index_length ) / ( 1024 *1024 ) AS size
              FROM information_schema.TABLES
              WHERE ENGINE=('MyISAM' || 'InnoDB' )
             AND table_schema = '#{get_current_db_name}'"
-    return  perform_sql_query(sql)
+    query_result = perform_sql_query(sql)
+    return  query_result[0][0].to_f
   end
 
+  # Return the name of the database that rails is currently using
   def self.get_current_db_name    
     return Rails.configuration.database_configuration[Rails.env]["database"]
   end
 
+  # Return the results of an SQL query.
   def self.perform_sql_query(query)
     result = []
     mysql_res = ActiveRecord::Base.connection.execute(query)
     mysql_res.each do |res|
       result << res
-      puts 'res:' + res.to_s
     end
-    return result[0][0].to_f
+    return result
   end
 
 end
