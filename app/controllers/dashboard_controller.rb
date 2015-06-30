f6e49f48391bd2635c350a450743ba0aceb43886
Charlie Martin <martinc@ucar.edu>
2015-06-30 06:16:14 -0600

    Add some comments.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,31 +1,30 @@
 class DashboardController < ApplicationController
   def index
 
-    start_time = Time.zone.now - 10.hour
-    @measurements_by_minute = Measurement.where("created_at >= ?", start_time).group("date_format(created_at, '%Y-%m-%dT%H:%i')").count
-
-    data_array = Array.new
-    @measurements_by_minute.each do |measurement_by_minute| 
-      iso8601 =      measurement_by_minute[0] + ':00+07:00'
-      miliseconds = (Time.iso8601(iso8601).to_f * 1000.0).to_i
-      str = '[' + miliseconds.to_s + ','+ measurement_by_minute[1].to_s + ']'
-      data_array.push(str)
-    end
-    
-    # 
+    # Summary metrics
     @metrics = {}
     @metrics["db_size_mb"]        = ApplicationHelper.total_db_size_mb
     @metrics["measurement_count"] = Measurement.count
     @metrics["site_count"]        = Site.count
     @metrics["instrument_count"]  = Instrument.count
     
+    # Create a table of number of measurements for each minute 
+    start_time = Time.zone.now - 10.hour
+    @measurements_by_minute = Measurement.where("created_at >= ?", start_time).group("date_format(created_at, '%Y-%m-%dT%H:%i')").count
+
+    data_array = Array.new
+    @measurements_by_minute.each do |measurement_by_minute| 
+      iso8601 =      measurement_by_minute[0] + ':00+07:00'
+      milliseconds = (Time.iso8601(iso8601).to_f * 1000.0).to_i
+      str = '[' + milliseconds.to_s + ','+ measurement_by_minute[1].to_s + ']'
+      data_array.push(str)
+    end
+    
     @measurements_by_minute_data = data_array.join(',')
     
+    # Create a table of number of measurements by hour
     start_time = Time.zone.now - 10.day
     @measurements_by_hour = Measurement.where("created_at >= ?", start_time).group("date_format(created_at, '%Y-%m-%dT%H')").count
-
-
-    # @measurements_by_hour = Measurement.group("date_format(created_at, '%Y-%m-%dT%H')").count
 
     data_array = Array.new
     @measurements_by_hour.each do |measurement_by_hour| 
@@ -36,7 +35,6 @@ class DashboardController < ApplicationController
     end
     
     @measurements_by_hour_data = data_array.join(',')
-
 
   end
 end
