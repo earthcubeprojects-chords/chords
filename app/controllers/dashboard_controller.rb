79a193481616c5c707cfc8ee4f6449bb1891a12a
Charlie Martin <martinc@ucar.edu>
2015-06-30 14:15:41 -0600

    Experiment with fetching sample counts for each instrument.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,19 +8,31 @@ class DashboardController < ApplicationController
     @metrics["site_count"]        = Site.count
     @metrics["instrument_count"]  = Instrument.count
     
-    # Create a table of number of measurements for each minute 
-    start_time = Time.zone.now - 10.hour
-    @measurements_by_minute = Measurement.where("created_at >= ?", start_time).group("date_format(created_at, '%Y-%m-%dT%H:%i')").count
-
-    data_array = Array.new
-    @measurements_by_minute.each do |measurement_by_minute| 
-      iso8601 =      measurement_by_minute[0] + ':00+07:00'
-      milliseconds = (Time.iso8601(iso8601).to_f * 1000.0).to_i
-      str = '[' + milliseconds.to_s + ','+ measurement_by_minute[1].to_s + ']'
-      data_array.push(str)
-    end
     
-    @measurements_by_minute_data = data_array.join(',')
+    # Get all of our instrument ids.
+    instrument_ids = Instrument.pluck(:id)
+    
+    # Create a table of number of measurements for each minute 
+    start_time = Time.zone.now - 2.hour
+    
+    data_arrays = []
+    j = 0
+    instrument_ids.each do |i|
+      measurements_by_minute = Measurement
+        .where("created_at >= ? and instrument_id = ?", start_time, i)
+        .group("date_format(created_at, '%Y-%m-%dT%H:%i')")
+        .count
+
+      data_arrays[j] = Array.new
+      measurements_by_minute.each do |measurement_by_minute| 
+        iso8601 =      measurement_by_minute[0] + ':00+07:00'
+        milliseconds = (Time.iso8601(iso8601).to_f * 1000.0).to_i
+        str = '[' + milliseconds.to_s + ','+ measurement_by_minute[1].to_s + ']'
+        data_arrays[j].push(str)
+      end
+      j += 1
+    end
+    @measurements_by_minute_data = data_arrays[0].join(',')
     
     # Create a table of number of measurements by hour
     start_time = Time.zone.now - 10.day
