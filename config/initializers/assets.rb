ce4e285d7b90f1e078f800ae8217fe76b97519dc
Charlie Martin <martinc@ucar.edu>
2015-07-08 15:40:02 -0600

    Add jqueryui, stylesheets and images.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( app.css )
 Rails.application.config.assets.precompile += %w( dashboard.css )
+Rails.application.config.assets.precompile += %w( datafetch.css )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
