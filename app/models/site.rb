a55fdb231ec7c3348d402a7dac5d6902d7de169c
Mike Dye <mdye@ucar.edu>
2015-07-21 22:47:57 -0600

    inplace site selection added

--- app/models/site.rb
+++ app/models/site.rb
@@ -7,4 +7,8 @@ class Site < ActiveRecord::Base
 #    Site.create([{name: 'ISS2 Sugarloaf', lat: '40.0', lon:'-104.1'}])
 #    Site.create([{name: 'ISS3 Nederland', lat: '39.9', lon:'-104.3'}])
   end
+  
+  def self.list_site_options 
+      Site.select("id, name").map {|site| [site.id, site.name] }
+  end  
 end
