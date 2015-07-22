7c04e800bb6d4713133c0196d2c26853096881af
Mike Dye <mdye@ucar.edu>
2015-07-21 21:45:52 -0600

    seeds added for acurite weater station

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,15 @@
 # Site.initialize
 # Instrument.initialize
 
+Site.create(id:8, name: 'Boulder Sugarloaf', lat: 40.0293,lon: -105.397 )
+Instrument.create(id: 21, name: 'Acurite Wx Station', site_id: 8, display_points: 100, seconds_before_timeout: 300)
+Var.create(id:9, name: 'Tdry', shortname: 'v1', instrument_id: 21)
+Var.create(id:10, name: 'RH', shortname: 'v2', instrument_id: 21)
+Var.create(id:11, name: 'Wspd', shortname: 'v3', instrument_id: 21)
+Var.create(id:12, name: 'Wdir', shortname: 'v4', instrument_id: 21)
+Var.create(id:13, name: 'Pres', shortname: 'v5', instrument_id: 21)
+Var.create(id:14, name: 'RainRate', shortname: 'v6', instrument_id: 21)
+Var.create(id:15, name: 'RainTotal', shortname: 'v7', instrument_id: 21)
+Var.create(id:16, name: 'DewPoint', shortname: 'v8', instrument_id: 21)
+
 
