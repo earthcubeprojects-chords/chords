3f9e7dd31e7ec0321c598fcd7671872c63926e72
Charlie Martin <martinc@ucar.edu>
2015-08-17 20:18:15 -0600

    Implemented user provided logos.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,7 @@
 class ProfilesController < ApplicationController
 # There will only be one record in the Profiles table
 
+  # GET /profiles
   def index
     @profile = Profile.first
     
@@ -11,24 +12,41 @@ class ProfilesController < ApplicationController
     
   end
   
+  # POST /profile
   def create
     project     = params[:profile][:project]
     affiliation = params[:profile][:affiliation]
     description = params[:profile][:description]
     timezone    = params[:profile][:timezone]
+    reset_logo  = params[:reset_logo].to_i
     
-    @profile = Profile.first
+    puts "reset_logo is #{reset_logo}"
+    
+    @profile = Profile.last
+    
+    if reset_logo == 1
+      logo = nil
+      puts "because reset_logo is #{reset_logo}"
+      puts 'reset logo'
+    else
+      uploaded_io = params[:profile][:logo]
+      if uploaded_io != nil
+        logo =  uploaded_io.read
+        puts 'got a logo'
+      end
+    end
     
     @profile.update(
       project: project, 
       affiliation: affiliation, 
       description: description,
-      timezone:    timezone
+      timezone:    timezone,
+      logo:        logo
       )
       
     flash[:notice] = 'Configuration saved.'
     
-    render "profiles/index"
+    redirect_to profiles_path
   end
   
 end
