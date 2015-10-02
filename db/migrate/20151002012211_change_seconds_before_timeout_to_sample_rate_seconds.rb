b70b769939b889446ecd621808b3122503c81949
Charlie Martin <martinc@ucar.edu>
2015-10-01 21:10:19 -0600

    Fix migration.

--- db/migrate/20151002012211_change_seconds_before_timeout_to_sample_rate_seconds.rb
+++ db/migrate/20151002012211_change_seconds_before_timeout_to_sample_rate_seconds.rb
@@ -1,5 +1,5 @@
 class ChangeSecondsBeforeTimeoutToSampleRateSeconds < ActiveRecord::Migration
   def change
-     rename_column :instruments, :sample_rate_seconds, :sample_rate_seconds
+     rename_column :instruments, :seconds_before_timeout, :sample_rate_seconds
   end
 end
