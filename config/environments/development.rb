3603db320e207f80f1b66fa6b5983b4f8c354bdc
Mike Dye <mdye@ucar.edu>
2015-10-27 13:35:28 -0600

    actionmailer default config set to  chord.dyndns.org

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,5 +40,6 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   
   # required by devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'chords.dyndns.org'}
 end
