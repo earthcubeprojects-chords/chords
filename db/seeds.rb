7b069b1edd5117d87ab1e86deea72e6e7382bf29
Mike Dye <mdye@ucar.edu>
2015-11-02 15:25:36 -0700

    test seeds removed before deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,19 +16,19 @@ User.create(:email => "mdye@ucar.edu",    :password => "realtimedata", :is_admin
 User.create(:email => "martinc@ucar.edu", :password => "realtimedata", :is_administrator => 1)
 User.create(:email => "daniels@ucar.edu", :password => "realtimedata", :is_administrator => 1)
 
-Site.create(id:7, name: 'Boulder ', lat: 40.029,lon: -105.337 )
-
-Site.create(id:8, name: 'Boulder Sugarloaf', lat: 40.0293,lon: -105.397 )
-
-Instrument.create(id: 21, name: 'Acurite Wx Station', site_id: 8, display_points: 100, sample_rate_seconds: 3)
-Var.create(id:9, name: 'Tdry', shortname: 'v1', instrument_id: 21)
-Var.create(id:10, name: 'RH', shortname: 'v2', instrument_id: 21)
-Var.create(id:11, name: 'Wspd', shortname: 'v3', instrument_id: 21)
-Var.create(id:12, name: 'Wdir', shortname: 'v4', instrument_id: 21)
-Var.create(id:13, name: 'Pres', shortname: 'v5', instrument_id: 21)
-Var.create(id:14, name: 'RainRate', shortname: 'v6', instrument_id: 21)
-Var.create(id:15, name: 'RainTotal', shortname: 'v7', instrument_id: 21)
-Var.create(id:16, name: 'DewPoint', shortname: 'v8', instrument_id: 21)
+# Site.create(id:7, name: 'Boulder ', lat: 40.029,lon: -105.337 )
+# 
+# Site.create(id:8, name: 'Boulder Sugarloaf', lat: 40.0293,lon: -105.397 )
+# 
+# Instrument.create(id: 21, name: 'Acurite Wx Station', site_id: 8, display_points: 100, sample_rate_seconds: 3)
+# Var.create(id:9, name: 'Tdry', shortname: 'v1', instrument_id: 21)
+# Var.create(id:10, name: 'RH', shortname: 'v2', instrument_id: 21)
+# Var.create(id:11, name: 'Wspd', shortname: 'v3', instrument_id: 21)
+# Var.create(id:12, name: 'Wdir', shortname: 'v4', instrument_id: 21)
+# Var.create(id:13, name: 'Pres', shortname: 'v5', instrument_id: 21)
+# Var.create(id:14, name: 'RainRate', shortname: 'v6', instrument_id: 21)
+# Var.create(id:15, name: 'RainTotal', shortname: 'v7', instrument_id: 21)
+# Var.create(id:16, name: 'DewPoint', shortname: 'v8', instrument_id: 21)
 
 
 # Using the ontology  http://sensorml.com/ont/swe/property
