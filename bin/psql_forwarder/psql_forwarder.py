6be32839d1cfd32f889f28b58b6aab95c5eb4677
Charlie Martin <martinc@ucar.edu>
2015-11-19 09:26:40 -0700

    Add http submission, test flag, and documentation. Code refator also.

--- bin/psql_forwarder/psql_forwarder.py
+++ bin/psql_forwarder/psql_forwarder.py
@@ -8,9 +8,17 @@ import psycopg2
 import pycurl
 import json
 import argparse
+import pycurl
+try:
+    from io import BytesIO
+except ImportError:
+    from StringIO import StringIO as BytesIO
 
 #####################################################################
 class CommandArgs:
+    # Manage the command line options.
+    # The options are collated in a dictionary keyed on the option long name.
+    # The option dictionary will only contain the options that are present on the command line.
     def __init__(self):
         parser = argparse.ArgumentParser()
         parser.add_argument("-f", "--config",      help="config file (required)")
@@ -20,6 +28,7 @@ class CommandArgs:
         parser.add_argument("-t", "--db_table",    help="database table")
         parser.add_argument("-c", "--chords_host", help="chords host")
         parser.add_argument("-k", "--key",         help="key")
+        parser.add_argument("-s", "--test",        help="add test flag", action="store_true")
         parser.add_argument("-v", "--verbose",     help="verbose output", action="store_true")
 
         args = parser.parse_args()
@@ -30,6 +39,7 @@ class CommandArgs:
             exit(1)
 
     def get_options(self):
+        # return the dictionary of existing options.
         return self.options 
 
 #####################################################################
@@ -51,7 +61,8 @@ class Config:
 
 #####################################################################
 class ADS_db:
-    # Manage the database connection
+    # Manage the database connection.
+    # All future transactions will be made against dbtable.
     def __init__(self, dbhost, dbname, dbuser, dbtable):
         self.dbname  = dbname
         self.dbuser  = dbuser
@@ -62,6 +73,7 @@ class ADS_db:
         self.cursor = conn.cursor()
     
     def list_columns(self):
+        # Return a list of olumn names,
         self.cursor.execute("select column_name from information_schema.columns where table_name = '" + self.dbtable +"';")
         return self.cursor.fetchall()
     
@@ -78,8 +90,10 @@ class ADS_db:
         return retval
 
     def get_values(self, cols):
-        # Execute a query for the specified columns
-        # cols is an array containing column names
+        # Execute a query for the specified columns. The 
+        # cursor will be set to the result.
+        # cols: a list containing column names
+        # return: Nothing
         sql = "select "
         i = 0
         for c in cols:
@@ -87,21 +101,25 @@ class ADS_db:
                 sql = sql + ','
             sql = sql + c
             i += 1
-        sql = sql + " from " + self.dbtable + ";"
+        sql = sql + " from " + self.dbtable + " limit 1;"
         self.cursor.execute(sql)
 
     def next_row(self):
+        # Get the next row at the cursor.
+        # Returns: a list of values.
         row = self.cursor.fetchone()
         return row
 
 #####################################################################
 class Measurement:
+    # Represent a CHORDS measurement.
     def __init__(self, short_name, value):
         self.short_name = short_name
         self.value_str = str(value)    
     
 #####################################################################
-def make_CHORDS_url(host, id, measurements=[], key=None, time=None):
+def make_CHORDS_url(host, id, measurements=[], key=None, time=None, test=None):
+    # Create a CHORDS URL.
     # instrument_id: string
     # measurements: an array of Measurement
     # key (optional): the security key (text)
@@ -113,10 +131,19 @@ def make_CHORDS_url(host, id, measurements=[], key=None, time=None):
         url += "&key=" + key 
     if time:
         url += "&at=" + time 
+    if test:
+        url += "&test"
     return url
 
 #####################################################################
 def option_override(name, options, config):
+    # Determine the final value of the configuration option.
+    # If the named item exists in options, that  will be returned. 
+    # Otherwise, if it exists in config, that will be returned.
+    # Else None will be returned.
+    # name: the option name
+    # options: a dictionary of option values.
+    # config: a dictionary of configuration values.
     retval = None
     if name in config:
         retval = config[name]
@@ -125,6 +152,74 @@ def option_override(name, options, config):
     return retval
 
 #####################################################################
+def http_GET(url):
+    # Send the URL
+    
+    buffer = BytesIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, 'http://pycurl.sourceforge.net/')
+    c.setopt(c.WRITEDATA, buffer)
+    c.perform()
+    
+    # HTTP response code, e.g. 200.
+    status = c.getinfo(c.RESPONSE_CODE)   
+    # getinfo must be called before close.
+    c.close()
+    
+    return status
+
+#####################################################################
+def verify_columns(db, vars):
+    # Verify that the specified columns exist in the database
+    # db: the database
+    # vars: a dictionary of column_name:short_name
+
+    # Extract the column names
+    columns = []
+    for col,short_name in vars.iteritems():
+        columns.append(col)
+    
+    missing_cols = db.verify_columns(columns)
+    if (len(missing_cols) > 0):
+        print "Columns",
+        for c in missing_cols:
+            print c + ",",
+        print "do not exist in database:" + db.dbname + ", table:" + db.dbtable
+        exit(1)
+
+#####################################################################
+def get_measurements(db, vars):
+    # Interogate the database for a row of values.
+    # Return these as a list of measurements
+    # db: the database
+    # vars: a dictionary of column_name:short_name
+    # Return: time, Measurement[]
+    
+    # Extract the column names
+    columns = []
+    for col,short_name in vars.iteritems():
+        columns.append(col)
+    
+    # Get a row for these columns
+    db.get_values(columns)
+    row = db.next_row()
+    
+    # Process the row
+    measurements = []
+    time = None
+    for c, v in zip(columns, row):
+        if c != time_col:
+            short_name = vars[c]
+            m = Measurement(short_name, v)
+            measurements.append(m)
+        else:
+            time = str(v).replace(" ","T")
+            
+    return time, measurements
+    
+#####################################################################
+# Get a row from the database, convert it to a URL, and send to a
+# CORDS Portal
 
 # Get the command line options
 options = CommandArgs().get_options()
@@ -136,6 +231,7 @@ config  = Config(options['config']).get_config()
 # If the value was not specified in either place, it is set to None
 chords_host = option_override('chords_host', options, config)
 key         = option_override('key',         options, config)
+test        = option_override('test',        options, config)
 db_name     = option_override('db_name',     options, config)
 db_host     = option_override('db_host',     options, config)
 db_user     = option_override('db_user',     options, config)
@@ -146,39 +242,36 @@ instrument_id = config['instrument_id']
 # The name of the column used for the timestamp
 time_col      = config['time_column']
 # config['var_short_names'] will be a dictionary of column_names:short_name entries.
-column_dict   = config['var_short_names']
+vars          = config['var_short_names']
+# Extract the column names
+columns = []
+for col,short_name in vars.iteritems():
+    columns.append(col)
 
 # Open the database
 db = ADS_db(dbhost=db_host, dbname=db_name, dbtable=db_table, dbuser=db_user)
 
-# Get the column names
-columns = []
-for col,short_name in column_dict.iteritems():
-    columns.append(col)
+# Make sure that the columns exist in the database. The program will
+# exit if they don't exist
+verify_columns(db, vars)
 
-# Make sure that the columns exist in the database
-missing_cols = db.verify_columns(columns)
-if (len(missing_cols) > 0):
-    print "Columns",
-    for c in missing_cols:
-        print c + ",",
-    print "do not exist in database:" + db.dbname + ", table:" + db.dbtable
-    exit(1)
+# Get a set of measurements from the database
+time, measurements = get_measurements(db, vars)
 
-# Get rows for these columns
-db.get_values(columns)
+# Make a url
+url = make_CHORDS_url(
+    host=chords_host, 
+    id=instrument_id, 
+    measurements=measurements, 
+    time=time,
+    key=key,
+    test=test)
 
-# Process a few of the rows
-for r in range(10):
-    row = db.next_row()
-    measurements = []
-    time = None
-    for c, v in zip(columns, row):
-        if c != time_col:
-            short_name = column_dict[c]
-            m = Measurement(short_name, v)
-            measurements.append(m)
-        else:
-            time = str(v).replace(" ","T")
-    url = make_CHORDS_url(host=chords_host, id=instrument_id, measurements=measurements, time=time, key=key)
+if options["verbose"]:
     print url
+
+# Send the url
+status = http_GET(url)
+if options["verbose"]:
+    print status
+
