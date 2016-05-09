d551cc1c716372c0c8e7f83052b3913f22c181f5
Mike Dye <mdye@ucar.edu>
2016-05-08 22:17:57 -0600

    unnecessary files removed

--- app/models/anonymous_user.rb
+++
@@ -1,8 +0,0 @@
-class AnonymousUser < User
-  attr_accessible *ACCESSIBLE_ATTRS, :type, :token, as: :registrant
-
-  def register(params)
-    params = params.merge(type: 'User', token: nil)
-    self.update_attributes(params, as: :registrant)
-  end
-end
