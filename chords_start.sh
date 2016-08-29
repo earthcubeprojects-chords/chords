f152608ad06b3ed2a838bd697be70df105e14a52
Charlie Martin <martinc@ucar.edu>
2016-08-29 12:18:20 -0700

    Make sure the log directory exists, and interate on database connection.

--- chords_start.sh
+++ chords_start.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 
-if [ !  -e "/chords_log/SEEDED" ]; then
-  echo "/chords_log/SEEDED not found. Will begin database creation in 10 seconds."
-  sleep 10
+server="mysql"
+seeded_flag="/chords_log/SEEDED"
+
+# Make sure that the log directory exists
+mkdir -p log
+
+# See if there is an existing database
+if [ ! -e $seeded_flag ] 
+then
+  echo "/chords_log/SEEDED not found. We will attempt to create the database."
+
+  for count in {1..60}; do
+    echo -n "..$count"
+    mysql -h $server -e "show databases;" >& /dev/null
+    if [ $? -eq 0 ]; then
+      echo
+      break
+    fi
+    
+    if [ $count -eq 60 ]; then
+      echo
+      echo "Could not contact the database server $server, aborting CHORDS app startup."
+      exit 1
+    fi
+    
+    sleep 1
+  done
+
   echo "Granting mysql permissions."
   bundle exec mysql -h mysql -e "GRANT ALL ON *.* TO 'chords_demo_user';"
+
   echo "Creating rails database."
   bundle exec rake db:create
 fi
@@ -12,11 +38,13 @@ fi
 echo "Migrating rails database."
 bundle exec rake db:migrate
 
-if [ !  -e "/chords_log/SEEDED" ]; then
+if [ !  -e $seeded_flag ]; then
   echo "/chords_log/SEEDED not found. Seeding rails database."
   bundle exec rake db:seed
-  touch chords_log/SEEDED
 fi
+
+# Database ready. Set the SEEDED flag.
+touch chords_log/SEEDED
 
 echo "Starting passsenger."
 passenger start --port 80
