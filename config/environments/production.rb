5a3bffcf389a435815c7a6e634b52a684c597dac
Charlie Martin <martinc@ucar.edu>
2016-09-30 20:31:08 -0700

    Set sendmail as the mailer.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+  config.action_mailer.delivery_method = :sendmail
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
