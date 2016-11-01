a0a9d319df744b36526d02206e9517581f23fe13
Charlie Martin <martinc@ucar.edu>
2016-11-01 15:26:32 -0600

    Add influxdb supporting funtions.

--- artifacts/influxdb/TSDB.rb
+++ artifacts/influxdb/TSDB.rb
@@ -1,9 +1,24 @@
 require 'influxdb'
+require 'json'
 
 class TSDB
 
-  def initialize(dbname, host)
-    @db = InfluxDB::Client.new dbname, host: host
+  def initialize(dbname, host, precision='ms')
+    
+    exists = false
+    databases = self.list_databases(host)
+    databases.each do |d|
+      if d["name"] == dbname then
+        exists = true
+      end
+    end
+    
+    if !exists then
+      self.create_database(dbname, host)
+      print "Database #{dbname} created on host #{host}\n"
+    end
+    
+    @db = InfluxDB::Client.new dbname, host: host, time_precision: precision
   end
   
   def query(q)
@@ -13,5 +28,25 @@ class TSDB
   def write(site, name, time, value)
     q_result = @db.query
     return q_result
+  end
+  
+  def write_point(data) 
+    w_result = @db.write_point(data)
+    return w_result
+  end
+
+  def write_points(data) 
+    w_result = @db.write_points(data)
+    return w_result
+  end
+
+  def list_databases(host)
+    db = InfluxDB::Client.new(host: host)
+    return db.list_databases
+  end
+
+  def create_database(dbname, host)
+    db = InfluxDB::Client.new(host: host)
+    db = db.create_database(dbname)
   end
 end 
