e3242bfbd8601270527f642be2c99354dc09d37b
Charlie Martin <martinc@ucar.edu>
2016-11-19 11:25:48 -0700

    Remove depricated function.

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -41,32 +41,6 @@ class Measurement < ActiveRecord::Base
     end
   end
 
-  def self.columns_with_metadata(varnames, metadata = {})
-    # Return an object of columnized Var data.
-    # Metadata may be included in the object.
-  
-    # Upon entry, we contain the measurements of interest (i.e.
-    # they have been time and instrument selected.
-    
-    # varnames are the names of parameters of interest.
-    # Metadata is a hash of metadata than will be prepended to the result.
-    
-    # Fetch the desired data. A hash is returned. It contains
-    # an entry "Time", with data times, and entries for each of the 
-    # varnames.
-    vardata = MeasurementsHelper.columnize(self, varnames)
-    
-    # Convert the nested hashes into vectors
-    retval = metadata
-    retval["Data"] = {}
-    vardata.each do |key, value|
-      retval["Data"][key] = value.values
-    end
-    
-    return retval
-
-  end
-
   # Return a vector containing time(ms) and value
   def mstime_and_value
     time = Time.new(self.measured_at.year, self.measured_at.month, self.measured_at.day, self.measured_at.hour, self.measured_at.min, self.measured_at.sec, "+00:00")
