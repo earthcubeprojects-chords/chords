4296c6acb367e4cd9ba5a5c7b580f782d500984d
Mike Dye <mdye@ucar.edu>
2017-06-01 00:21:16 -0600

    rake task built to create test data set for n points per variable

--- app/models/var.rb
+++ app/models/var.rb
@@ -12,6 +12,56 @@ class Var < ActiveRecord::Base
     return self.shortname + '_at'
   end
 
+  def random_value(previous_value = nil)
+
+    # the maximum percent a previous value can change
+    maximum_percent_change = 0.05
+    
+    # the number of decimal places to return
+    precision = 2
+    
+    # default min/max values
+    minimum = 0
+    maximum = 100
+    
+
+    # set a min/max if the are devined for this variable
+    if self.minimum_plot_value
+      minimum = self.minimum_plot_value
+    end
+
+    if self.maximum_plot_value
+      maximum = self.maximum_plot_value
+    end
+
+    # if a previous value exists, adjust that value +/- withing the percentage range
+    if previous_value
+      maximum_change = ((maximum - minimum)*(maximum_percent_change))
+
+      # set the possible min/max range for the change
+      local_minimum = (previous_value - maximum_change)
+      local_maximum = (previous_value + maximum_change)
+
+      # set the new random cvalue
+      change = maximum_change/2 - rand(maximum_change)
+      random_value = previous_value + change
+
+      # make sure the returned values are still within the min/max bounds
+      # if not, adjust accordingly
+      if (random_value < minimum)
+        random_value =  previous_value - change
+      end
+
+      if (random_value > maximum)
+        random_value =  previous_value - change
+      end
+
+    else 
+      random_value = (minimum + rand(maximum)).to_f
+    end
+
+    return random_value.round(precision)
+  end
 
   
   def get_tspoints (since, display_points = self.instrument.display_points)
