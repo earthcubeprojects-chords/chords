7b0d2de9b08f888e0fb34336337fa2d6c1dcb7f7
Mike Dye <mdye@ucar.edu>
2017-06-13 20:28:12 -0600

    data import/export scripts added for influxdb

--- bin/influxdb/export_influxdb_tsdata_file.sh
+++ bin/influxdb/export_influxdb_tsdata_file.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-influx_inspect export -database chords_ts_development -datadir /var/lib/influxdb/data -waldir /var/lib/influxdb/wal -out /tmp/chords-influxdb-backup
+influx_inspect export -database chords_ts_development -datadir /var/lib/influxdb/data -waldir /var/lib/influxdb/wal -out /tmp/chords-influxdb-backup -compress
