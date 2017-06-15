2752816cdcd00f172b52980ddf6d989088226051
Mike Dye <mdye@ucar.edu>
2017-06-15 15:54:09 -0600

    influxdb import script now uses environment variables in command line passwor

--- bin/influxdb/import_influxdb_tsdata_file.sh
+++ bin/influxdb/import_influxdb_tsdata_file.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-influx -username admin -password chords_ec_demo -database chords_ts_development  -import -compressed -precision=ns -path=/tmp/chords-influxdb-backup
+influx -username $INFLUXDB_USERNAME -password $INFLUXDB_PASSWORD -database chords_ts_development  -import -compressed -precision=ns -path=/tmp/chords-influxdb-backup
