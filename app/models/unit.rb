f14c16bcd7aa345f5646f2c8e9598bd5289948dd
terrieyang <terrieyang@berkeley.edu>
2017-06-16 17:16:49 +0000

    Revert units

--- app/models/unit.rb
+++
@@ -1,7 +0,0 @@
-class Unit < ActiveRecord::Base
-	has_many :vars
-  
-  	validates :name, uniqueness: { case_sensitive: false }
-  	validates :abbreviation, uniqueness: { case_sensitive: false }
-
-end
