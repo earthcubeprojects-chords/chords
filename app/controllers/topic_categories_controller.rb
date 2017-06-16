74dd4939aaeb19c54445e8c83e708e7ff751604b
terrieyang <tyang48459@gmail.com>
2017-06-16 13:25:59 -0600

    show a topic category

--- app/controllers/topic_categories_controller.rb
+++ app/controllers/topic_categories_controller.rb
@@ -5,4 +5,9 @@ class TopicCategoriesController < ApplicationController
     @topic_categories = TopicCategory.all
   end
 
+  def show
+  	@topic_category = TopicCategory.find(params[:id])
+  end
+
+
 end
