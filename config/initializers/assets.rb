5dc281118ab1f8e2d3a7477351c86c939796266c
terrieyang <tyang48459@gmail.com>
2017-07-05 11:43:25 -0600

    autocomplete jquery

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,6 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( app.css )
 Rails.application.config.assets.precompile += %w( dashboard.css )
 Rails.application.config.assets.precompile += %w( data.css )
-# Rails.application.config.assets.precompile += %w( autocomplete.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
