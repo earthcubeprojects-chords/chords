6c533cfd59545f9982e211319295740bdddf5ac3
Mike Dye <mdye@ucar.edu>
2017-07-18 12:21:08 -0600

    CUAHSI profile contact metadata added to schema

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -185,8 +185,14 @@ class ProfilesController < ApplicationController
       params.require(:profile).permit(
         :project, :affiliation, :page_title, :description, :logo, :created_at, :updated_at, :timezone, 
         :secure_administration, :secure_data_viewing, :secure_data_download, 
-        :secure_data_entry, :data_entry_key, :google_maps_key, :backup_file, :doi
+        :secure_data_entry, :data_entry_key, :google_maps_key, :backup_file, :doi,
+        :contact_name, :contact_phone, :contact_email, :contact_address, :contact_city, :contact_zipcode
         )
     end
 
 end
+
+
+
+
+
---
+++ db/migrate/20170718180231_add_cuahsi_profile_metadata.rb
@@ -0,0 +1,10 @@
+class AddCuahsiProfileMetadata < ActiveRecord::Migration
+  def change
+  	add_column :profiles, :contact_name, :string, null: false, :default => 'Contact Name'
+  	add_column :profiles, :contact_phone, :string, null: false, :default => 'Contact Phone'
+  	add_column :profiles, :contact_email, :string, null: false, :default => 'Contact Email'
+  	add_column :profiles, :contact_address, :string, null: false, :default => 'Contact Address'
+  	add_column :profiles, :contact_city, :string, null: false, :default => 'Contact City'
+  	add_column :profiles, :contact_zipcode, :string, null: false, :default => 'Contact Zipcode'
+  end
+end
