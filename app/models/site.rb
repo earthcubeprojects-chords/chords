d72208010d21a3f25b44823b011c9e30c661afec
terrieyang <tyang48459@gmail.com>
2017-07-24 15:19:33 -0600

    get cuahsi sitecode

--- app/models/site.rb
+++ app/models/site.rb
@@ -5,8 +5,21 @@ class Site < ActiveRecord::Base
   end
   
   def self.list_site_options 
-      Site.select("id, name").map {|site| [site.id, site.name] }
+    Site.select("id, name").map {|site| [site.id, site.name] }
   end  
   
+  def self.get_cuahsi_sitecode
+  	uri = URI.parse("http://hydroportal.cuahsi.org/CHORDS/index.php/default/services/api/GetSitesJSON")
+
+    request = Net::HTTP::Post.new uri.path
+
+    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => false) do |http|
+      response = http.request request
+    end
+    puts response.body
+    sites = JSON.parse(response.body)
+    code = sites.count
+    return code + 1
+  end
   
 end
