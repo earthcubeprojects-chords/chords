a82e3662e8686189aed74ab38464b8909e0b81e9
Mike Dye <mdye@ucar.edu>
2017-07-24 17:16:02 -0600

    cleanup code

--- config/initializers/archive.rb
+++ config/initializers/archive.rb
@@ -1,3 +1,2 @@
 # Read in and parse the config/archive.yml configuration file
-ARCHIVE_CONFIG = Rails.application.config_for(:archive)
-Rails.application.config.x.archive = ARCHIVE_CONFIG
+Rails.application.config.x.archive = Rails.application.config_for(:archive)
