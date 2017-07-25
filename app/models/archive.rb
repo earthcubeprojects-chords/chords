fb5c96502863b7d5b3d0da43cf82d6df95e540ca
Mike Dye <mdye@ucar.edu>
2017-07-25 16:40:55 -0600

    send_frequency options added

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -14,4 +14,19 @@ class Archive < ActiveRecord::Base
     options = { :none =>  'none', :CUAHSI => 'CUAHSI' }
   end
   
+  
+  def self.send_frequency_options
+    
+    options = Hash.new
+    options['1.months'] = 'every month'
+    options['1.weeks'] = 'every week'
+    options['1.days'] = 'every day'
+    options['6.hours'] = 'every 6 hours'
+    options['1.hours'] = 'every hour'
+    options['15.minutes'] = 'every 15 minutes'
+    options['5.minutes'] = 'every 5 minutes'
+    options['1.minutes'] = 'every minute'
+    
+    return options
+  end
 end
