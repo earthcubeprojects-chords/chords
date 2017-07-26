af73deb57a0d550f5dee3015327997500239aab4
Mike Dye <mdye@ucar.edu>
2017-07-26 17:23:13 -0600

    get first tspoint code modified ot return global first point if no instrument id is specified

--- app/services/get_first_ts_point.rb
+++ app/services/get_first_ts_point.rb
@@ -5,13 +5,21 @@ class GetFirstTsPoint
   # time_series_db - the database.
   # field          - the field to query for..
   # inst_id        - the instrument id.
-  def self.call(time_series_db, field, inst_id)
+  def self.call(time_series_db, field, inst_id=nil)
     
-    ts_point = time_series_db \
-      .select("*") \
-      .where("inst = '#{inst_id}'") \
-      .order("asc") \
-      .limit(1)
+    if (inst_id == nil)
+      ts_point = time_series_db \
+        .select("*") \
+        .order("asc") \
+        .limit(1)
+
+    else
+      ts_point = time_series_db \
+        .select("*") \
+        .where("inst = '#{inst_id}'") \
+        .order("asc") \
+        .limit(1)      
+    end
     
     return ts_point
   end  
