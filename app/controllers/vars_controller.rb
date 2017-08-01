b5905c5211bcf74400adcf86295d94576f4f91b9
terrieyang <tyang48459@gmail.com>
2017-08-01 14:31:51 -0600

    link buttons to controller actions

--- app/controllers/vars_controller.rb
+++ app/controllers/vars_controller.rb
@@ -78,6 +78,17 @@ class VarsController < ApplicationController
     end
   end
 
+  def push_cuahsi_variables
+    Var.all.each do |var|
+      data = var.create_cuahsi_variable
+      if var.get_cuahsi_variableid(data["VariableCode"]) == nil
+        uri_path = Rails.application.config.x.archive['base_url'] + "/default/services/api/variables"
+        CuahsiHelper::send_request(uri_path, data)
+        var.get_cuahsi_variableid(data["VariableCode"])
+      end
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_var
