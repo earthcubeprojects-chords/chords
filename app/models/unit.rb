9af1d721abdbca76fd3ed2d9724c5021e761747c
terrieyang <tyang48459@gmail.com>
2017-08-01 16:02:43 -0600

    import cuahsi unit CV

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -6,19 +6,25 @@ class Unit < ActiveRecord::Base
 		validates :name, uniqueness: { case_sensitive: false }
 
 
-  	def self.populate
-  		csv_file = Rails.root + 'lib/assets/udunits.csv'
-  		csv_text = File.read(csv_file)
-			csv = CSV.parse(csv_text, :headers => true)
-			csv.each do |row|
-				fields = row.to_hash
-				unit = Unit.new
-				unit.name = fields["name"]
-				unit.abbreviation = fields["abbreviation"]
-				unit.save
-			end
-  	end
-
-
+	def self.populate
+		xml_file = Rails.root + 'lib/assets/cuahsi_units.xml'
+  	doc = File.open(xml_file) { |f| Nokogiri::XML(f) }
+  	doc.xpath("//x:Record", {"x" => "http://his.cuahsi.org/his/1.1/ws/"}).each do |node|
+      params = Hash.new
+      node.children.each do |property_node| 
+      	case property_node.name
+            when 'UnitID'
+              params['unit_id'] = property_node.text
+            when 'UnitName'            
+              params['name'] = property_node.text
+            when 'UnitType'            
+              params['type'] = property_node.text
+            when 'UnitAbbreviation'
+            	params['abbreviation'] = property_node.text
+       	end
+    	Unit.create(params)
+    	end
+    end
+	end
 
 end
