ca6fa289a59adce3fbc56486d87b48687f849aab
Mike Dye <mdye@ucar.edu>
2017-08-07 13:56:55 -0600

    relationship between instrument and topic categories changed to :has_may <=> :belongs_to

--- db/migrate/20170615020238_create_join_table_instrument_topic_category.rb
+++ db/migrate/20170615020238_create_join_table_instrument_topic_category.rb
@@ -1,8 +1,5 @@
 class CreateJoinTableInstrumentTopicCategory < ActiveRecord::Migration
   def change
-    create_join_table :instruments, :topic_categories do |t|
-      # t.index [:instrument_id, :topic_category_id]
-      # t.index [:topic_category_id, :instrument_id]
-    end
+    add_column :instruments, :topic_category_id, :integer
   end
 end
