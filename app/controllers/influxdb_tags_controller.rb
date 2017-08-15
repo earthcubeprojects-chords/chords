bcedadb70493ee1137b0c0652940c82153f56424
Mike Dye <mdye@ucar.edu>
2017-08-14 23:29:45 -0600

    auth for influxdb tags

--- app/controllers/influxdb_tags_controller.rb
+++ app/controllers/influxdb_tags_controller.rb
@@ -14,16 +14,21 @@ class InfluxdbTagsController < ApplicationController
 
   # GET /influxdb_tags/new
   def new
+    authorize! :manage, Instrument
+
     @influxdb_tag = InfluxdbTag.new
   end
 
   # GET /influxdb_tags/1/edit
   def edit
+    authorize! :manage, Instrument
   end
 
   # POST /influxdb_tags
   # POST /influxdb_tags.json
   def create
+    authorize! :manage, Instrument
+    
     @influxdb_tag = InfluxdbTag.new(influxdb_tag_params)
 
     respond_to do |format|
@@ -41,6 +46,8 @@ class InfluxdbTagsController < ApplicationController
   # PATCH/PUT /influxdb_tags/1
   # PATCH/PUT /influxdb_tags/1.json
   def update
+    authorize! :manage, Instrument
+
     respond_to do |format|
       if @influxdb_tag.update(influxdb_tag_params)
         format.html { redirect_to @influxdb_tag, notice: 'Influxdb tag was successfully updated.' }
@@ -55,6 +62,8 @@ class InfluxdbTagsController < ApplicationController
   # DELETE /influxdb_tags/1
   # DELETE /influxdb_tags/1.json
   def destroy
+    authorize! :manage, Instrument
+    
     @influxdb_tag.destroy
     respond_to do |format|
       # format.html { redirect_to influxdb_tags_url, notice: 'Influxdb tag was successfully destroyed.' }
