fd6a6daca103bdf359b1507fb9b0e09858b67d24
Mike Dye <mdye@ucar.edu>
2017-08-14 21:53:10 -0600

    default domain name changed to example.chordsrt.come

--- db/migrate/20170724212745_add_domain_name_to_profile.rb
+++ db/migrate/20170724212745_add_domain_name_to_profile.rb
@@ -1,5 +1,5 @@
 class AddDomainNameToProfile < ActiveRecord::Migration
   def change
-  	add_column :profiles, :domain_name, :string, null: false, :default => 'portal.chordsrt.com'
+  	add_column :profiles, :domain_name, :string, null: false, :default => 'example.chordsrt.com'
   end
 end
