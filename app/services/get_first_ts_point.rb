80ba3e5c5eb7c14c06b3b6619ba48bff0670b420
MisterMartin <martinc@ucar.edu>
2017-09-19 09:10:22 -0600

    Correct and improve comments.

--- app/services/get_first_ts_point.rb
+++ app/services/get_first_ts_point.rb
@@ -1,21 +1,22 @@
 
 class GetFirstTsPoint
-  # Return the last time series point.
+  # Return the first time series point.
+  # if no instrument id is specified, return 
+  # the first point for all instruments.
   #
   # time_series_db - the database.
   # field          - the field to query for..
   # inst_id        - the instrument id.
   def self.call(time_series_db, field, inst_id=nil)
     
-    # if not instrument id is specified, return the first point in the entire series
     if (inst_id == nil)
+      # if not instrument id is specified, return the first point in the entire series
       ts_point = time_series_db \
         .select("*") \
         .order("asc") \
         .limit(1)
-
-    # if instrument id is set, return the first point for that instrument
     else
+      # if instrument id is set, return the first point for that instrument
       ts_point = time_series_db \
         .select("*") \
         .where("inst = '#{inst_id}'") \
