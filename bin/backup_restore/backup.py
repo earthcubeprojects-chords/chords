0562742fad4b80614546913de109073883bc41c1
MisterMartin <martinc@ucar.edu>
2019-02-25 13:06:45 -0700

    Create one file containg influx and mysql.

--- bin/backup_restore/backup.py
+++ bin/backup_restore/backup.py
@@ -9,31 +9,34 @@ if __name__ == '__main__':
     
     time_stamp = datetime.datetime.now().replace(microsecond=0).isoformat()
     
-    mysql_backp_file = 'mysql-'+time_stamp
-
-    influx_dump_dir = '/tmp/influxdb-'+time_stamp
-    influx_tarfile = influx_dump_dir+'.tgz'
+    mysql_dump_file = 'mysql-'+time_stamp+'.sql'
+    influx_dump_file = 'influxdb-'+time_stamp+'.tar'
+    chords_dump_file = 'chords-'+time_stamp+'.tgz'
 
     print("*** Dump mysql ***")
     docker_cmds = [
         'exec -t chords_mysql /usr/bin/mysqldump chords_demo_production'
     ]
-    mysql_file = open(mysql_backp_file, 'w')
+    mysql_file = open(mysql_dump_file, 'w')
     print(sh.docker(docker_cmds[0].split(), _out=mysql_file).stdout)
-    print(sh.gzip(mysql_backp_file).stdout)
     print("*********************")
     print("")
 
     print("***Dump influxdb ***")
+    influx_dump_dir = '/tmp/influxdb-'+time_stamp
     docker_cmds = [
-        'ps -a',
-        'exec -t chords_influxdb sh -c ls -l /tmp',
+        'exec -t chords_influxdb ls -l /tmp',
         'exec -t chords_influxdb influxd backup -database chords_ts_production %s' %(influx_dump_dir),
-        'exec -t chords_influxdb tar -cvzf %s %s' % (influx_tarfile, influx_dump_dir),
-        'cp chords_influxdb:%s .' % (influx_tarfile),
-        'exec -t chords_influxdb rm %s' % (influx_tarfile),
+        'exec -t chords_influxdb tar --force-local -cvf %s %s' % (influx_dump_file, influx_dump_dir),
+        'cp chords_influxdb:%s .' % (influx_dump_file),
+        'exec -t chords_influxdb rm %s' % (influx_dump_file),
         'exec -t chords_influxdb rm -rf %s' % (influx_dump_dir)
     ]
-
     for cmd in docker_cmds:
         print(sh.docker(cmd.split()).stdout)
+    print("*********************")
+    print("")
+
+    print(sh.tar('-cvf', chords_dump_file, mysql_dump_file, influx_dump_file))
+    print(sh.rm(mysql_dump_file, influx_dump_file))
+
