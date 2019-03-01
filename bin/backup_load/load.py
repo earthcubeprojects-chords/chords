eeb96911648cfe5e45766d59550f687709ec3259
MisterMartin <martinc@ucar.edu>
2019-03-01 15:24:40 -0700

    refacto LoadChords.

--- bin/backup_load/load.py
+++ bin/backup_load/load.py
@@ -7,6 +7,7 @@ import tempfile
 import glob
 from collections import namedtuple
 import sh
+import docker
 
 class LoadChords:
     """
@@ -17,17 +18,34 @@ class LoadChords:
         """
         """
         self.dump_file = dump_file
-        self.tmp_dir = tempfile.mkdtemp(prefix="chords-", dir=os.path.normpath(tmp_dir))
+        self.tmp_dir = tempfile.mkdtemp(prefix="chords_load-", dir=os.path.normpath(tmp_dir))
         self.file_paths = {}
-        self.unpack()
+        self.backup_unpack()
         self.valid = self.validate()
+        if self.valid:
+            return
+        
+        self.load_mysql()
 
-    def unpack(self):
+        self.load_influxdb()
+
+    def load_mysql(self, container="chords_mysql", database_name="chords_demo_production"):
         """
-        Docs.
+        Load the mysql database.
         """
-        print("*** Unpacking chords files ***")
-        print(sh.tar('-xzvf', self.dump_file, '-C', self.tmp_dir, _err_to_out=True).stdout)
+        print("*** Loading mysql ***")
+        print ("Container:%s, database:%s" % (container, database_name))
+        docker_args = ['exec', '-i', container, '/usr/bin/mysql', database_name]
+        print(sh.docker(sh.cat(self.file_paths["mysql"]), docker_args, _err_to_out=True).stdout)
+
+    def load_influxdb(self, container="chords_influxdb", database_name="chords_ts_production"):
+        """
+        Load the influxdb database.
+        """
+        print("*** Loading influxdb ***")
+        print ("Container:%s, database:%s" % (container, database_name))
+        #docker_args = ['exec', '-i', container, '/usr/bin/mysql', database_name]
+        #print(sh.docker(sh.cat(self.file_paths["mysql"]), docker_args, _err_to_out=True).stdout)
 
     def err(self):
         """
@@ -37,14 +55,24 @@ class LoadChords:
 
     def validate(self):
         """
-        Verify that one and only one valid dump file exists for each database.
+        Perform validation checks.
 
+         - Verify that the docker apps are correct
+         - Verify that one and only one valid dump file exists for each database.
+        """
+        retval = ""
+        retval += self.docker_check()
+        retval += self.file_check()
+        return retval
+
+    def file_check(self):
+        """
         Determine the dump file names for each database.
 
         Return an empty string if valid, or an error message if not.
         """
-        FileSpec = namedtuple('FileSpec', ['prefix', 'ext'])
         retval = ""
+        FileSpec = namedtuple('FileSpec', ['prefix', 'ext'])
         file_types = [FileSpec('mysql', 'sql'),
                       FileSpec('influxdb', 'tar')]
         for file_type in file_types:
@@ -63,12 +91,26 @@ class LoadChords:
                     retval += "No %s dump file was found." % (file_type.prefix)
         return retval
 
-    def load_mysql(self, container="chords_mysql", database_name="chords_demo_production"):
+    def docker_check(self):
         """
-        Load the mysql database.
+        Verify that a CHORDS instance is running correctly.
         """
-        docker_args = ['exec', '-i', container, '/usr/bin/mysql', database_name]
-        print(sh.docker(sh.cat(self.file_paths["mysql"]), docker_args, _err_to_out=True).stdout)
+        client = docker.from_env()
+        our_containers = [c.name for c in client.containers.list()]
+        msg = ""
+        for c in ["chords_app", "chords_influxdb", "chords_mysql"]:
+            if c not in our_containers:
+                msg += "The %s docker container is not present.\n" % (c)
+        return msg
+
+    def backup_unpack(self):
+        """
+        Docs.
+        """
+        print("*** Unpacking chords files ***")
+        print("Temporary directory: " + self.tmp_dir)
+        print(sh.tar('-xzvf', self.dump_file, '-C', self.tmp_dir, _err_to_out=True).stdout)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -81,10 +123,6 @@ if __name__ == "__main__":
 
     err_msg = load_chords.err()
     if err_msg:
-        print(chords_dump_file + ": " + err_msg)
+        print(err_msg)
         exit(1)
 
-    print("*** Loading mysql ***")
-    load_chords.load_mysql()
-
-    print("*** Loading influxdb ***")
