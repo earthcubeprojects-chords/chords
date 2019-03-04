f584a1c2bc9525f8ff7426d003664a8a55ceb829
MisterMartin <martinc@ucar.edu>
2019-03-04 16:28:19 -0700

    Streamlining backup script.

--- bin/backup_load/backup.py
+++ bin/backup_load/backup.py
@@ -5,6 +5,19 @@ import sys
 import datetime
 import sh
 
+def docker_bash(container, script):
+    """
+    Run shell comands in bash. This allows multiple
+    commands to be &&'ed together
+    """
+    print(sh.docker('exec', '-t', container, '/bin/bash', '-c', script, _err_to_out=True).stdout)
+
+def docker_cp(src, dest):
+    """
+    Copy file to/from ontainer.
+    """
+    print(sh.docker('cp', src, dest, _err_to_out=True).stdout)
+    
 #####################################################################
 
 if __name__ == '__main__':
@@ -20,24 +33,12 @@ if __name__ == '__main__':
     mysql_file = open(mysql_dump_file, 'w')
     print(sh.docker('exec -t chords_mysql /usr/bin/mysqldump chords_demo_production'.split(),
                     _out=mysql_file).stdout)
-    print("")
 
     print("*** Saving influxdb ***")
-    print(sh.docker('exec', '-t', 'chords_influxdb',
-                    'ls', '-l', '/tmp',
-                    _err_to_out=True).stdout)
-    print(sh.docker('exec', '-t', 'chords_influxdb',
-                    '/bin/bash', '-c', "cd /tmp && influxd backup -portable %s" % (influx_dump_dir),
-                    _err_to_out=True).stdout)
-    print(sh.docker('exec', '-t', 'chords_influxdb',
-                    '/bin/bash', '-c', "cd /tmp && tar -cvf %s %s" % (influx_tar_file, influx_dump_dir),
-                    _err_to_out=True).stdout)
-    print(sh.docker('cp',
-                    'chords_influxdb:%s' % ("/tmp/"+influx_tar_file), '.',
-                    _err_to_out=True).stdout)
-    print(sh.docker('exec', '-t', 'chords_influxdb',
-                    '/bin/bash', '-c', "cd /tmp && rm -rf %s %s" % (influx_tar_file, influx_dump_dir),
-                    _err_to_out=True).stdout)
+    docker_bash("chords_influxdb", "cd /tmp && influxd backup -portable %s" % (influx_dump_dir))
+    docker_bash("chords_influxdb", "cd /tmp && tar -cvf %s %s" % (influx_tar_file, influx_dump_dir))
+    docker_cp('chords_influxdb:%s' % ("/tmp/"+influx_tar_file), '.')
+    docker_bash("chords_influxdb", "cd /tmp && rm -rf %s %s" % (influx_tar_file, influx_dump_dir))
 
     print("*** Packaging files ***")
     tar_params = ['-cvf', chords_tar_file, mysql_dump_file, influx_tar_file]
