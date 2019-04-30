930124115faa709df03c661862db8376550b05c9
Aaron Botnick <botnick@ucar.edu>
2019-04-30 13:59:56 -0600

    Rework user ability specs after changes for measurements/downloader users

--- spec/unit/user_ability_spec.rb
+++ spec/unit/user_ability_spec.rb
@@ -116,8 +116,15 @@ describe 'User' do
       it{ allow_read_only(Unit) }
     end
 
-    context 'when is only a downloader' do
+    context 'when is only a downloader and secure viewing and downloading enabled' do
       let(:user) { FactoryBot.create(:data_downloader, roles: [:downloader]) }
+
+      before do
+        profile = Profile.first || Profile.initialize.first
+        profile.secure_data_download = true
+        profile.secure_data_viewing = true
+        profile.save!
+      end
 
       # Site
       it{ no_access_allowed(Site) }
@@ -194,8 +201,99 @@ describe 'User' do
       it{ no_access_allowed(Unit) }
     end
 
-    context 'when is only a measurement creator' do
+    context 'when is only a downloader and secure viewing and downloading disabled' do
+      let(:user) { FactoryBot.create(:data_downloader, roles: [:downloader]) }
+
+      before do
+        profile = Profile.first || Profile.initialize.first
+        profile.secure_data_download = false
+        profile.secure_data_viewing = false
+        profile.save!
+      end
+
+      # Site
+      it{ allow_read_only(Site) }
+      it{ is_expected.to be_able_to(:map, Site) }
+      it{ is_expected.to be_able_to(:map_markers_geojson, Site) }
+      it{ is_expected.to be_able_to(:map_balloon_json, Site) }
+
+      # Instrument
+      it{ allow_read_only(Instrument) }
+
+      it{ is_expected.not_to be_able_to(:duplicate, Instrument) }
+      it{ is_expected.to be_able_to(:live, Instrument) }
+      it{ is_expected.not_to be_able_to(:simulator, Instrument) }
+
+      # Variable
+      it{ allow_read_only(Var) }
+
+      # Measurement Creation
+      it{ is_expected.not_to be_able_to(:create, :measurement) }
+      it{ is_expected.not_to be_able_to(:delete_test, :measurement) }
+
+      # Profile
+      it{ no_access_allowed(Profile) }
+      it{ is_expected.not_to be_able_to(:export_configuration, Profile) }
+      it{ is_expected.not_to be_able_to(:import_configuration, Profile) }
+      it{ is_expected.not_to be_able_to(:export_influxdb, Profile) }
+      it{ is_expected.not_to be_able_to(:import_influxdb, Profile) }
+      it{ is_expected.not_to be_able_to(:test_sending_email, Profile) }
+
+      # User
+      it{ allow_read_update(User) }
+      it{ is_expected.to be_able_to(:assign_api_key, user) }
+
+      it 'cannot read other users' do
+        user2 = FactoryBot.create(:user)
+
+        is_expected.to be_able_to(:read, user)
+        is_expected.not_to be_able_to(:read, user2)
+      end
+
+      it 'cannot update other users' do
+        user2 = FactoryBot.create(:user)
+
+        is_expected.to be_able_to(:update, user)
+        is_expected.not_to be_able_to(:update, user2)
+      end
+
+      # About
+      it{ is_expected.to be_able_to(:read, :about) }
+
+      # Dashboard
+      it{ is_expected.to be_able_to(:read, :dashboard) }
+
+      # Data Download
+      it{ is_expected.to be_able_to(:read, :data) }
+      it{ is_expected.to be_able_to(:download, Instrument) }
+
+      # Influxdb Tag
+      it{ allow_read_only(InfluxdbTag) }
+
+      # Linked Data
+      it{ no_access_allowed(LinkedDatum) }
+
+      # Measured Property
+      it{ allow_read_only(MeasuredProperty) }
+
+      # Site Type
+      it{ allow_read_only(SiteType) }
+
+      # Topic Category
+      it{ allow_read_only(TopicCategory) }
+
+      # Unit
+      it{ allow_read_only(Unit) }
+    end
+
+    context 'when is only a measurement creator and secure viewing and downloading enabled' do
       let(:user) { FactoryBot.create(:data_creator) }
+
+      before do
+        profile = Profile.first || Profile.initialize
+        profile.secure_data_viewing = true
+        profile.save!
+      end
 
       # Site
       it{ no_access_allowed(Site) }
@@ -208,7 +306,7 @@ describe 'User' do
 
       it{ is_expected.not_to be_able_to(:duplicate, Instrument) }
       it{ is_expected.not_to be_able_to(:live, Instrument) }
-      it{ is_expected.not_to be_able_to(:simulator, Instrument) }
+      it{ is_expected.to be_able_to(:simulator, Instrument) }
 
       # Variable
       it{ no_access_allowed(Var) }
@@ -270,6 +368,91 @@ describe 'User' do
 
       # Unit
       it{ no_access_allowed(Unit) }
+    end
+
+    context 'when is only a measurement creator and secure viewing and downloading disabled' do
+      let(:user) { FactoryBot.create(:data_creator) }
+
+      before do
+        profile = Profile.first || Profile.initialize.first
+        profile.secure_data_download = false
+        profile.secure_data_viewing = false
+        profile.save!
+      end
+
+      # Site
+      it{ allow_read_only(Site) }
+      it{ is_expected.to be_able_to(:map, Site) }
+      it{ is_expected.to be_able_to(:map_markers_geojson, Site) }
+      it{ is_expected.to be_able_to(:map_balloon_json, Site) }
+
+      # Instrument
+      it{ allow_read_only(Instrument) }
+
+      it{ is_expected.not_to be_able_to(:duplicate, Instrument) }
+      it{ is_expected.to be_able_to(:live, Instrument) }
+      it{ is_expected.to be_able_to(:simulator, Instrument) }
+
+      # Variable
+      it{ allow_read_only(Var) }
+
+      # Measurement Creation
+      it{ is_expected.to be_able_to(:create, :measurement) }
+      it{ is_expected.not_to be_able_to(:delete_test, :measurement) }
+
+      # Profile
+      it{ no_access_allowed(Profile) }
+      it{ is_expected.not_to be_able_to(:export_configuration, Profile) }
+      it{ is_expected.not_to be_able_to(:import_configuration, Profile) }
+      it{ is_expected.not_to be_able_to(:export_influxdb, Profile) }
+      it{ is_expected.not_to be_able_to(:import_influxdb, Profile) }
+      it{ is_expected.not_to be_able_to(:test_sending_email, Profile) }
+
+      # User
+      it{ allow_read_update(User) }
+      it{ is_expected.to be_able_to(:assign_api_key, user) }
+
+      it 'cannot read other users' do
+        user2 = FactoryBot.create(:user)
+
+        is_expected.to be_able_to(:read, user)
+        is_expected.not_to be_able_to(:read, user2)
+      end
+
+      it 'cannot update other users' do
+        user2 = FactoryBot.create(:user)
+
+        is_expected.to be_able_to(:update, user)
+        is_expected.not_to be_able_to(:update, user2)
+      end
+
+      # About
+      it{ is_expected.to be_able_to(:read, :about) }
+
+      # Dashboard
+      it{ is_expected.to be_able_to(:read, :dashboard) }
+
+      # Data Download
+      it{ is_expected.to be_able_to(:read, :data) }
+      it{ is_expected.to be_able_to(:download, Instrument) }
+
+      # Influxdb Tag
+      it{ allow_read_only(InfluxdbTag) }
+
+      # Linked Data
+      it{ no_access_allowed(LinkedDatum) }
+
+      # Measured Property
+      it{ allow_read_only(MeasuredProperty) }
+
+      # Site Type
+      it{ allow_read_only(SiteType) }
+
+      # Topic Category
+      it{ allow_read_only(TopicCategory) }
+
+      # Unit
+      it{ allow_read_only(Unit) }
     end
 
     context 'when is a site configurator' do
@@ -677,7 +860,7 @@ describe 'User' do
 
       it{ is_expected.not_to be_able_to(:duplicate, Instrument) }
       it{ is_expected.to be_able_to(:live, Instrument) }
-      it{ is_expected.not_to be_able_to(:simulator, Instrument) }
+      it{ is_expected.to be_able_to(:simulator, Instrument) }
 
       # Variable
       it{ allow_read_only(Var) }
