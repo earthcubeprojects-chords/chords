b7ede6519663962bcbc001e3e154f60d72fd1cc2
Mike Dye <mike@mikedye.com>
2019-08-09 00:58:06 -0600

    renewal ssl certificate script

---
+++ bin/ssl_renew.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/usr/local/bin/docker-compose -f /home/ec2-user/chords/docker-compose.yml run certbot renew --dry-run \
+&& /usr/local/bin/docker-compose -f /home/ec2-user/chords/docker-compose.yml kill -s SIGHUP webs
