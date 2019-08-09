3ac4f8196c465e1c0435c119a87ac8d3182266bc
Mike Dye <mike@mikedye.com>
2019-08-09 01:55:01 -0600

    ssl cert renrewal script switched in to live mode

--- bin/ssl/renew_ssl_cert.sh
+++ bin/ssl/renew_ssl_cert.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-/usr/local/bin/docker-compose -f /home/ec2-user/chords/docker-compose.yml run certbot renew --dry-run \
+cd /home/ec2-user/chords
+
+# /usr/local/bin/docker-compose -f /home/ec2-user/chords/docker-compose.yml run certbot renew --dry-run \
+# && /usr/local/bin/docker-compose -f /home/ec2-user/chords/docker-compose.yml kill -s SIGHUP web
+
+
+/usr/local/bin/docker-compose -f /home/ec2-user/chords/docker-compose.yml run certbot renew \
 && /usr/local/bin/docker-compose -f /home/ec2-user/chords/docker-compose.yml kill -s SIGHUP web
