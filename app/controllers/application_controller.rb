ef046cad2d24eb150d530b84047beafc57d46b4e
Mike Dye <mike@mikedye.com>
2019-08-13 00:24:03 -0600

    forgery protection check is disabled for bulk_create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, except: :bulk_create
 
   before_action :set_global, :set_access_control_header
 
