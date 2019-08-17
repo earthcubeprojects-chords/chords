082d43c9370f32bff377615201f87e583cca8528
Mike Dye <mike@mikedye.com>
2019-08-17 04:36:50 -0600

    validation added for the bulk upload variable shortnames

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -11,10 +11,13 @@ class Measurement
   attr_accessor :variable_shortname
   attr_accessor :value
   attr_accessor :is_test_value
+  attr_accessor :instrument
 
   # Calculated attributes
+  attr_accessor :variable_id
   attr_accessor :timestamp
   attr_accessor :tags
+
 
   def initialize(instrument, measurement_json, is_test_value)
 
@@ -24,6 +27,10 @@ class Measurement
     self.value = measurement_json['value'].to_f
     self.measured_at = measurement_json['measured_at']
     self.is_test_value = is_test_value
+    self.instrument = instrument
+
+
+    self.variable_id = instrument.get_var_id_by_var_shortname(self.variable_shortname)
 
 
     self.timestamp = begin
@@ -37,7 +44,7 @@ class Measurement
     self.tags = instrument.influxdb_tags_hash
     self.tags[:site] = instrument.site_id
     self.tags[:inst] = instrument.id
-    self.tags[:var]  = instrument.get_var_id_by_var_shortname(self.variable_shortname)
+    self.tags[:var]  = self.variable_id
     self.tags[:test] = self.is_test_value
 
   end
@@ -46,9 +53,15 @@ class Measurement
   def validate!
 
     if (self.timestamp.nil?)
-      create_err_msg = "Time format error, the timestamp '#{self.measured_at}' violates ISO8601 formatting."
+      err_msg = "Time format error, the timestamp '#{self.measured_at}' violates ISO8601 formatting."
       # errors.add(:timestamp, :blank, message: create_err_msg)
-      errors.add(:timestamp, :invalid, message: create_err_msg)
+      errors.add(:timestamp, :invalid, message: err_msg)
+    end
+
+    if (self.variable_id.nil?)
+      err_msg = "Variable ID error, the variable shortname '#{self.variable_shortname}' is not defined for the instrument with id '#{self.instrument.id}'."
+      # errors.add(:timestamp, :blank, message: create_err_msg)
+      errors.add(:variable_id, :invalid, message: err_msg)
     end
 
   end
