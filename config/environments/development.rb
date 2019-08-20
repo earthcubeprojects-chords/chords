23631fea8e3697099138e464bb0e1234d75db271
Mike Dye <mike@mikedye.com>
2019-08-20 02:33:17 -0600

    development environment email sender changed from :letter_opener to :smtp.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
 
   config.action_mailer.default_options = {from: from_email}
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     address:               ENV['CHORDS_EMAIL_SERVER'],
