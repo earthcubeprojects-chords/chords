89a1860a8e00419d8506abde9fde9ac014950f2c
MisterMartin <MisterMartin@users.noreply.github.com>
2019-12-09 17:06:31 -0700

    Starting chored_control integration.

---
+++ bin/certbot/certbot_start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Certbot conditional startup
+#
+# Set SSL_ENABLED to true or TRUE to neable startup of certbot.
+# Certbot will be started in a repeating certificate renewal mode.
+#
+
+if [ -z "$SSL_ENABLED" ]; then
+  export SSL_ENABLED="false"
+fi
+
+if [ $SSL_ENABLED == "TRUE" ]; then
+  export SSL_ENABLED="true"
+fi
+
+# If enabled, start capacitor
+if [ $SSL_ENABLED == "true" ]; then
+  echo "*** SSL is enabled; starting certbot in certificate renewal mode"
+  /bin/sh -c 'trap exit TERM; while :; do certbot renew; sleep 6h & wait ${!}; done;'
+else
+  echo "SSL was not enabled (via SSL_ENABLED); certbot is not starting"
+fi
